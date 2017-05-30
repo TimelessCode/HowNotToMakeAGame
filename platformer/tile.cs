e545ec0567133c175861fcf3a09baa92830b8dd8
TimelessCode <brychikwendu@gmail.com>
2017-05-30 19:03:35 +0100

    Added entry point to add files

---
+++ platformer/tile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeldaLikeSpace
+{
+   public class tile
+    {
+        public tile(Rectangle rf,type t) { r = rf;mtype = t; }
+
+       public Rectangle r;
+      public  enum type
+        {
+            door,
+            norm,
+            enemy,
+        }
+
+        public type mtype;
+    }
+}
